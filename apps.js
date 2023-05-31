f4bf1e99521b33100bf293f30235bd1c76f379ac
Pretty-Reemah <132362476+Pretty-Reemah@users.noreply.github.com>
2023-05-31 14:47:14 +0100

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,11 @@
+ let url = "https://randomuser.me/api/";
+ const text1 = document.getElementsByClassName('text2');
+const text2 = document.getElementsByClassName('text3');
+const text3 = document.getElementsByClassName('text4');
+const text4 = document.getElementsByClassName('text5');
+const text5 = document.getElementsByClassName('text6');
+
+//const text = document.querySelectorAll('h1')
+//const btn = document.getElementById('button');
+
+console.log(text);
